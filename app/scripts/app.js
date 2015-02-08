5d64481c4f45e707f2709ac7266a45dd28f246c9
born_free <b0rn@list.ru>
2015-02-08 15:38:52 +0300

    check that internet is turned on

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,8 +13,23 @@ angular.module('Simpleweek', [
   'angular-momentjs'
 ])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $ionicPopup) {
   $ionicPlatform.ready(function() {
+
+    if(window.Connection) {
+      if(navigator.connection.type == Connection.NONE) {
+        $ionicPopup.confirm({
+          title: "Internet Disconnected",
+          content: "The internet is disconnected on your device."
+        })
+          .then(function(result) {
+            if(!result) {
+              ionic.Platform.exitApp();
+            }
+          });
+      }
+    }
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
